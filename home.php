8e4308891a182e08d0949c94f7030555f8e20dca
severinlx <severinlx@yahoo.com>
2020-12-11 14:24:27 +0100

    Update home.php

--- home.php
+++ home.php
@@ -14,32 +14,11 @@
 
 <main>
    <?php snippet('intro') ?>
-<?php
-     $myPDO = new PDO('sqlite:/var/www/html/site/templates/Vakanzengrabber.db');
-     $results = $myPDO->query("Select * From freelancede_2020_10_30 UNION Select * From freelancermap_2020_10_30");
-	 /**
-	 Wenn das Resultat nicht leer ist und ein array ist, wird die ausgabe vorgenommen
-	 **/
-	 if (is_array($results) || is_object($results))
-	{
-		print "<table>\n";
-		print "<tr><th>ID</th><th>Titel</th><th>Start</th><th>Ende</th></tr>";
-		 foreach($results as $result)
-		 {
-			 print "<tr><td>" . $result[0] . "<td>";
-			 print "<td>" . $result[1] . "<td>";
-			 print "<td>" . $result[2] . "</td>";
-			 print "<td>" . $result[4] . "</td></tr>";
-			 //print "<br>";
-		 }
-	print "</table>";
-  }
-	else
-	{
-		print "Keine Resultate";
-	}
-?>
-
+<kirby-form-field>
+  <input kirby-input placeholder="Suchbegriff" name="Suchbegriff" type="Suchbegriff"/>
+  <button kirby-button>Suchen</button>
+</kirby-form-field>
+   <?php snippet('search') ?>
 
 </main>
 
